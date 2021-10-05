d517f33cfebf721db81c6d7a5442fb9f7a9abaf3
GuidoBarilatti <barilattiguidoa@hotmail.com>
2021-10-05 11:49:01 -0300

    add three vertical dots in business-list.php

--- Views/business-list.php
+++ Views/business-list.php
@@ -24,8 +24,10 @@
                                              <td><?php echo $business->getBusinessInfo(); ?></td>
                                              <td>
                                                   <div class="dropdown">
-                                                       <button class="btn btn-secondary" type="button" id="dropdownoptions" data-toggle= "dropdown" aria-extended= "true">opciones
-                                                       <i class="bi bi-three-dots-vertical"></i>
+                                                       <button class="btn btn-primary" type="button" id="dropdownoptions" data-toggle= "dropdown" aria-extended= "true">
+                                                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                                             <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                                       </svg>
                                                        </button>
                                                        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownoptions">
                                                             <li role="presentation"><a href= "<?php echo FRONT_ROOT?>Business/DeleteBusiness?$id=<?php echo $business->getBusinessId(); ?>">Eliminar</a></li>
