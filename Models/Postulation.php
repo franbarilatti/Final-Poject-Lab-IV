9cdcb0019b9c43bf9d1f2d1402bd4a7963727a2e
Franco <francoagustinbarilatti@hotmail.com>
2021-10-05 15:31:18 -0300

    add DAO to class Postulation

--- Models/Postulation.php
+++ Models/Postulation.php
@@ -1,4 +1,95 @@
 <?php
     namespace Models;
 
+    class Postulation{
+
+        private $postulationId;
+        private $student;
+        private $business;
+        private $jobPosition;
+        private $active;
+
+        public function __construct()
+        {
+            $this->postulationId = rand(1000000,9999999);
+            $this->active = true;
+        }
+
+
+        ////////// GETTERS METHODS //////////
+
+        
+        public function getPostulationId()
+        {
+                return $this->postulationId;
+        }
+
+        
+        public function getStudent()
+        {
+                return $this->student;
+        }
+
+        
+        public function getBusiness()
+        {
+                return $this->business;
+        }
+
+         
+        public function getJobPosition()
+        {
+                return $this->jobPosition;
+        }
+
+        public function getActive()
+        {
+                return $this->active;
+        }
+
+        ////////// GETTERS METHODS //////////
+        
+        public function setPostulationId($postulationId)
+        {
+                $this->postulationId = $postulationId;
+
+                return $this;
+        }
+
+        
+        
+        public function setStudent($student)
+        {
+                $this->student = $student;
+
+                return $this;
+        }
+
+        
+        
+        public function setBusiness($business)
+        {
+                $this->business = $business;
+
+                return $this;
+        }
+
+        
+
+        public function setJobPosition($jobPosition)
+        {
+                $this->jobPosition = $jobPosition;
+
+                return $this;
+        }
+
+       
+        public function setActive($active)
+        {
+                $this->active = $active;
+
+                return $this;
+        }
+    } 
+
 ?>
