30a17a5a4c9c361242a5aae5ff27232726d0b479
Franco <francoagustinbarilatti@hotmail.com>
2021-10-10 17:48:54 -0300

    add business-profile and redirections

--- Views/business-profile.php
+++ Views/business-profile.php
@@ -1,22 +1,12 @@
-<?php
 
-    use Config\Autoload as Autoload;
-    require_once("Config/Autoload.php");
-    
-    Autoload::Start();
-    
-    use Models\Business as Business;
-
-    $business = $_SESSION['business'];
-    echo var_dump($business);
-
-?>
-
-<nav >
-
-
-</nav>
 <main>
+    <div>
+        <h1><?php echo $business->getBusinessName();?></h1>
+    </div>
+    <div>
+        <h3>Cantidad de empleados: <?php echo $business->getEmployesQuantity();?></h3>
+        <p><?php echo $business->getBusinessInfo();?></p>
+    </div>
 
 
 
